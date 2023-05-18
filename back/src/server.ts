2cea533ac7e0ade26e5f103c8a1c9e4d1f30a69d
diegorogerssa <rogersmhdg@gmail.com>
2023-05-18 15:21:45 -0300

    autenticação de rotas, +jwt

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,11 +1,20 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(cors, {
   origin: true,
 })
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
