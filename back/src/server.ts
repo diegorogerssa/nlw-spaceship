f153a6db3890e11eb99b447709c229d3cef04b98
diegorogerssa <rogersmhdg@gmail.com>
2023-05-19 17:44:02 -0300

    back: implementação de upload de arquivos

--- back/src/server.ts
+++ back/src/server.ts
@@ -4,16 +4,24 @@ import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+app.register(multipart)
 app.register(jwt, {
   secret: 'spacetime',
 })
-
 app.register(cors, {
   origin: true,
 })
+app.register(uploadRoutes)
 app.register(authRoutes)
 app.register(memoriesRoutes)
 
