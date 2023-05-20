54db0a254404fbbdea4803d1198c65baa3000e2a
diegorogerssa <rogersmhdg@gmail.com>
2023-05-20 19:56:51 -0300

    web: criação de nova memoria e enviando para o banco de dados

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -1,4 +1,5 @@
-import { Camera, ChevronLeft } from 'lucide-react'
+import { NewMemoryForm } from '@/components/NewMemoryForm'
+import { ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
 
 export default function newMemory() {
@@ -11,40 +12,7 @@ export default function newMemory() {
         <ChevronLeft className="h-4 w-4" />
         voltar à timeline
       </Link>
-
-      <form className="flex flex-1 flex-col gap-2">
-        <div className="flex items-center gap-4">
-          <label
-            className="flex cursor-pointer items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
-            htmlFor="media"
-          >
-            <Camera className="h-4 w-4" />
-            Anexar mídia
-          </label>
-          <label
-            className="flex cursor-pointer items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
-            htmlFor="isPublic"
-          >
-            <input
-              className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500 focus:ring-0"
-              type="checkbox"
-              name="isPublic"
-              id="isPublic"
-              value="true"
-            />
-            tornar memória pública
-          </label>
-        </div>
-
-        <input type="file" id="media" className="invisible h-0 w-0" />
-
-        <textarea
-          name="content"
-          spellCheck={false}
-          className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
-          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
-        />
-      </form>
+      <NewMemoryForm />
     </div>
   )
 }
